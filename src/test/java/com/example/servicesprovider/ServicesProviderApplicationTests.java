94a6ae8367adddd6893c8c3dcddac37a3ed23d1a
saber <sbaerm.com@gmail.com>
2023-10-16 02:24:47 +0330

    start the project

---
+++ src/test/java/com/example/servicesprovider/ServicesProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
