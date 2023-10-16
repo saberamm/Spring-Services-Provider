a03987ae54aa17fee20e13730b27ebd972280b8b
saber <sbaerm.com@gmail.com>
2023-10-16 16:55:06 +0330

    add User class

---
+++ src/main/java/com/example/servicesprovider/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.servicesprovider.service;
+
+import com.example.servicesprovider.base.service.BaseService;
+import com.example.servicesprovider.model.User;
+
+public interface UserService extends BaseService<User, Long> {
+
+    User findByUserName(String userName);
+
+    void changePassword(String userName, String password, String newPassword, String duplicateNewPassword);
+
+    User userAuthentication(String userName, String password);
+}
---
+++ src/main/java/com/example/servicesprovider/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.servicesprovider.service.impl;
+
+import com.example.servicesprovider.base.service.impl.BaseServiceImpl;
+import com.example.servicesprovider.model.User;
+import com.example.servicesprovider.repository.UserRepository;
+import com.example.servicesprovider.service.UserService;
+
+import javax.validation.Validator;
+
+public class UserServiceImpl extends BaseServiceImpl<User, Long, UserRepository> implements UserService {
+
+
+    public UserServiceImpl(UserRepository repository, Validator validator) {
+        super(repository, validator);
+    }
+
+    @Override
+    public User findByUserName(String userName) {
+        try {
+            return repository.findByUserName(userName);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void changePassword(String userName, String password, String newPassword, String duplicateNewPassword) {
+        User user = userAuthentication(userName, password);
+        try {
+            user.setPassword(newPassword);
+            update(user);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public User userAuthentication(String userName, String password) {
+        try {
+            return repository.findByUserNameAndPassword(userName, password);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+}
