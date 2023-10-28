4e89b577089c93fa72e79476192ef96cabaa7a07
saber <sbaerm.com@gmail.com>
2023-10-29 02:56:16 +0330

    add save method to client controller

--- src/main/java/com/example/servicesprovider/config/AppConfig.java
+++ src/main/java/com/example/servicesprovider/config/AppConfig.java
@@ -1,7 +1,13 @@
 package com.example.servicesprovider.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/example/servicesprovider/controller/ClientController.java
+++ src/main/java/com/example/servicesprovider/controller/ClientController.java
@@ -1,18 +1,24 @@
 package com.example.servicesprovider.controller;
 
+import com.example.servicesprovider.dto.ClientRequestDto;
+import com.example.servicesprovider.dto.ClientResponseDto;
 import com.example.servicesprovider.model.Client;
+import com.example.servicesprovider.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/client")
+@AllArgsConstructor
 public class ClientController {
+    ClientService clientService;
+    ModelMapper modelMapper;
 
     @GetMapping("/find/{username}")
     public ResponseEntity<Client> getClient(@PathVariable String username) {
@@ -21,5 +27,13 @@ public class ClientController {
                 .password("1wW#qwer").email("qwe@a4sd.com").
                 clientCredit(50000D).nationalCode("5555555555").phoneNumber("09999999999").build();
         return new ResponseEntity<>(client, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ClientResponseDto> addClient(@RequestBody @Valid ClientRequestDto clientRequestDto) {
+        Client client = modelMapper.map(clientRequestDto, Client.class);
+        Client savedClient = clientService.save(client);
+        ClientResponseDto clientResponseDto = modelMapper.map(savedClient, ClientResponseDto.class);
+        return new ResponseEntity<>(clientResponseDto, HttpStatus.CREATED);
     }
 }
