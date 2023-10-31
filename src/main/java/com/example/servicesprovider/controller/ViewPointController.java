a21ffe4577c924bf13114364333a357010dfc8b0
saber <sbaerm.com@gmail.com>
2023-11-01 02:50:06 +0330

    add view point controller

--- src/main/java/com/example/servicesprovider/controller/ViewPointController.java
+++ src/main/java/com/example/servicesprovider/controller/ViewPointController.java
@@ -1,11 +1,49 @@
 package com.example.servicesprovider.controller;
 
+import com.example.servicesprovider.dto.ViewPointRequestDto;
+import com.example.servicesprovider.dto.ViewPointResponseDto;
+import com.example.servicesprovider.mapper.ViewPointMapper;
+import com.example.servicesprovider.model.ViewPoint;
+import com.example.servicesprovider.service.ViewPointService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/viewpoint")
 @AllArgsConstructor
 public class ViewPointController {
+    ViewPointMapper viewPointMapper;
+    ViewPointService viewPointService;
+
+    @GetMapping("/find/{viewPointId}")
+    public ResponseEntity<ViewPointResponseDto> getViewPoint(@PathVariable Long viewPointId) {
+        ViewPoint viewPoint = viewPointService.findById(viewPointId);
+        ViewPointResponseDto viewPointResponseDto = viewPointMapper.map(viewPoint);
+        return new ResponseEntity<>(viewPointResponseDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ViewPointResponseDto> addViewPoint(@RequestBody @Valid ViewPointRequestDto viewPointRequestDto) {
+        ViewPoint viewPoint = viewPointMapper.map(viewPointRequestDto);
+        ViewPoint savedViewPoint = viewPointService.save(viewPoint);
+        ViewPointResponseDto viewPointResponseDto = viewPointMapper.map(savedViewPoint);
+        return new ResponseEntity<>(viewPointResponseDto, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{viewPointId}")
+    public void deleteViewPoint(@PathVariable Long viewPointId) {
+        viewPointService.deleteById(viewPointId);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ViewPointResponseDto> updateViewPoint(@RequestBody @Valid ViewPointRequestDto viewPointRequestDto) {
+        ViewPoint viewPoint = viewPointService.findById(viewPointRequestDto.getId());
+        viewPointMapper.map(viewPointRequestDto, viewPoint);
+        ViewPoint updatedViewPoint = viewPointService.update(viewPoint);
+        ViewPointResponseDto viewPointResponseDto = viewPointMapper.map(updatedViewPoint);
+        return new ResponseEntity<>(viewPointResponseDto, HttpStatus.CREATED);
+    }
 }
