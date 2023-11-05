175ddb9ab9e77277a15edcbae3d096d9be2d60db
saber <sbaerm.com@gmail.com>
2023-11-05 18:18:54 +0330

    replace foreach with stream in filtering method in admin controller

--- src/main/java/com/example/servicesprovider/controller/AdminController.java
+++ src/main/java/com/example/servicesprovider/controller/AdminController.java
@@ -12,14 +12,10 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -144,17 +140,8 @@ public class AdminController {
             @RequestParam(defaultValue = "firstName") String sortBy,
             Pageable pageable
     ) {
-        Page<User> users = userService.searchAndFilterUsers(role, firstName, lastName, email,aboutMe, sortBy, pageable);
-
-        List<UserResponseDto> usersDtoList = new ArrayList<>();
-
-        for (User user : users) {
-            UserResponseDto userDto = modelMapper.map(user, UserResponseDto.class);
-            usersDtoList.add(userDto);
-        }
-
-        Page<UserResponseDto> usersDto = new PageImpl<>(usersDtoList, pageable, users.getTotalElements());
-
+        Page<User> users = userService.searchAndFilterUsers(role, firstName, lastName, email, aboutMe, sortBy, pageable);
+        Page<UserResponseDto> usersDto = users.map(user -> modelMapper.map(user, UserResponseDto.class));
         return new ResponseEntity<>(usersDto, HttpStatus.OK);
     }
 }
