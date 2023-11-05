b25889d2dfed9146f3bfddbae5319e13cdca15f4
saber <sbaerm.com@gmail.com>
2023-11-05 03:40:32 +0330

    add model mapper to TechnicianMapper

--- src/main/java/com/example/servicesprovider/mapper/TechnicianMapper.java
+++ src/main/java/com/example/servicesprovider/mapper/TechnicianMapper.java
@@ -1,10 +1,13 @@
 package com.example.servicesprovider.mapper;
 
 import com.example.servicesprovider.dto.TechnicianRequestDto;
+import com.example.servicesprovider.dto.TechnicianResponseDto;
 import com.example.servicesprovider.model.Technician;
 
 public interface TechnicianMapper {
     Technician map(TechnicianRequestDto technicianRequestDto);
+
+    TechnicianResponseDto map(Technician technician);
 
     void map(TechnicianRequestDto technicianRequestDto, Technician technician);
 }
--- src/main/java/com/example/servicesprovider/mapper/impl/TechnicianMapperImpl.java
+++ src/main/java/com/example/servicesprovider/mapper/impl/TechnicianMapperImpl.java
@@ -5,75 +5,31 @@ import com.example.servicesprovider.mapper.TechnicianMapper;
 import com.example.servicesprovider.model.Technician;
 import com.example.servicesprovider.utility.ImageConverter;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class TechnicianMapperImpl implements TechnicianMapper {
     ImageConverter imageConverter;
+    ModelMapper modelMapper;
 
     @Override
     public Technician map(TechnicianRequestDto technicianRequestDto) {
         if (technicianRequestDto == null) return null;
-        Technician technician = new Technician();
-        technician.setId(technicianRequestDto.getId());
-        technician.setFirstName(technicianRequestDto.getFirstName());
-        technician.setLastName(technicianRequestDto.getLastName());
-        technician.setUserName(technicianRequestDto.getUserName());
-        technician.setPassword(technicianRequestDto.getPassword());
-        technician.setBirthDate(technicianRequestDto.getBirthDate());
-        technician.setEmail(technicianRequestDto.getEmail());
-        technician.setTechnicianStatus(technicianRequestDto.getTechnicianStatus());
-        technician.setNegativeScore(technicianRequestDto.getNegativeScore());
-        technician.setOverallScore(technicianRequestDto.getOverallScore());
-        technician.setPhoneNumber(technicianRequestDto.getPhoneNumber());
-        technician.setNationalCode(technicianRequestDto.getNationalCode());
-        technician.setAboutMe(technicianRequestDto.getAboutMe());
+        Technician technician = modelMapper.map(technicianRequestDto, Technician.class);
         technician.setTechnicianPhoto(imageConverter.convertMultipartFileToFile(technicianRequestDto.getTechnicianPhoto()));
         return technician;
     }
 
     @Override
+    public TechnicianResponseDto map(Technician technician) {
+        return null;
+    }
+
+    @Override
     public void map(TechnicianRequestDto technicianRequestDto, Technician technician) {
-        if (technicianRequestDto.getId() != null) {
-            technician.setId(technicianRequestDto.getId());
-        }
-        if (technicianRequestDto.getFirstName() != null) {
-            technician.setFirstName(technicianRequestDto.getFirstName());
-        }
-        if (technicianRequestDto.getLastName() != null) {
-            technician.setLastName(technicianRequestDto.getLastName());
-        }
-        if (technicianRequestDto.getUserName() != null) {
-            technician.setUserName(technicianRequestDto.getUserName());
-        }
-        if (technicianRequestDto.getPassword() != null) {
-            technician.setPassword(technicianRequestDto.getPassword());
-        }
-        if (technicianRequestDto.getBirthDate() != null) {
-            technician.setBirthDate(technicianRequestDto.getBirthDate());
-        }
-        if (technicianRequestDto.getEmail() != null) {
-            technician.setEmail(technicianRequestDto.getEmail());
-        }
-        if (technicianRequestDto.getTechnicianStatus() != null) {
-            technician.setTechnicianStatus(technicianRequestDto.getTechnicianStatus());
-        }
-        if (technicianRequestDto.getNegativeScore() != null) {
-            technician.setNegativeScore(technicianRequestDto.getNegativeScore());
-        }
-        if (technicianRequestDto.getOverallScore() != null) {
-            technician.setOverallScore(technicianRequestDto.getOverallScore());
-        }
-        if (technicianRequestDto.getPhoneNumber() != null) {
-            technician.setPhoneNumber(technicianRequestDto.getPhoneNumber());
-        }
-        if (technicianRequestDto.getNationalCode() != null) {
-            technician.setNationalCode(technicianRequestDto.getNationalCode());
-        }
-        if (technicianRequestDto.getAboutMe() != null) {
-            technician.setAboutMe(technicianRequestDto.getAboutMe());
-        }
+        modelMapper.map(technicianRequestDto, technician);
         if (technicianRequestDto.getTechnicianPhoto() != null) {
             technician.setTechnicianPhoto(imageConverter.convertMultipartFileToFile(technicianRequestDto.getTechnicianPhoto()));
         }
