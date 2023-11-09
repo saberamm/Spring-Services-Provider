4e46d9f02bb31ab2c5bc2087027bd460f7432111
saber <sbaerm.com@gmail.com>
2023-11-10 02:27:20 +0330

    add slf4j to log errors

--- src/main/java/com/example/servicesprovider/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/servicesprovider/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.example.servicesprovider.exception;
 
 import com.example.servicesprovider.utility.ErrorDetails;
-import jakarta.persistence.PersistenceException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -13,31 +13,24 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(PersistenceException.class)
-    public ResponseEntity<String> PersistenceExceptionHandler(PersistenceException e) {
-        return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
-    }
-    @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> IOExceptionHandler(IOException e) {
-        return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
-    }
-
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> RuntimeExceptionHandler(RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
+    public ResponseEntity<Object> RuntimeExceptionHandler(RuntimeException e) {
+        log.error(e.getMessage());
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), e.getMessage(), 400);
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                 @Nullable HttpHeaders headers,
-                                                                 @Nullable HttpStatusCode status,
-                                                                 @Nullable WebRequest request) {
+                                                                  @Nullable HttpHeaders headers,
+                                                                  @Nullable HttpStatusCode status,
+                                                                  @Nullable WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 "TotalErrors:" + ex.getErrorCount() + " First Error:" + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(),
                 400);
