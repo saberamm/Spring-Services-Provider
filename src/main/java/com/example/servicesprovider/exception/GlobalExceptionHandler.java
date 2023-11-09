c5508f30ed536825785887c364890e96dd0cb794
saber <sbaerm.com@gmail.com>
2023-11-10 02:08:10 +0330

    add nullable to handleMethodArgumentNotValid method

--- src/main/java/com/example/servicesprovider/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/servicesprovider/exception/GlobalExceptionHandler.java
@@ -2,11 +2,11 @@ package com.example.servicesprovider.exception;
 
 import com.example.servicesprovider.utility.ErrorDetails;
 import jakarta.persistence.PersistenceException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,30 +18,26 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @ControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(PersistenceException.class)
     public ResponseEntity<String> PersistenceExceptionHandler(PersistenceException e) {
-        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
     }
     @ExceptionHandler(IOException.class)
     public ResponseEntity<String> IOExceptionHandler(IOException e) {
-        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> RuntimeExceptionHandler(RuntimeException e) {
-        log.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.OK).body(e.getMessage());
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatusCode status,
-                                                                  WebRequest request) {
+                                                                 @Nullable HttpHeaders headers,
+                                                                 @Nullable HttpStatusCode status,
+                                                                 @Nullable WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
                 "TotalErrors:" + ex.getErrorCount() + " First Error:" + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage(),
                 400);
