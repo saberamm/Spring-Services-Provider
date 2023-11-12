f9516d878b6d357ebdfd4840b9534c63f8fd0819
saber <sbaerm.com@gmail.com>
2023-11-12 19:43:37 +0330

    add searchAndFilterUsers to order service

--- src/main/java/com/example/servicesprovider/service/OrderService.java
+++ src/main/java/com/example/servicesprovider/service/OrderService.java
@@ -1,11 +1,18 @@
 package com.example.servicesprovider.service;
 
 import com.example.servicesprovider.base.service.BaseService;
+import com.example.servicesprovider.dto.OrderFilterRequestDto;
 import com.example.servicesprovider.model.Order;
 import com.example.servicesprovider.model.SubService;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public interface OrderService extends BaseService<Order, Long> {
     List<Order> findBySubService(SubService subService);
+
+    @Transactional
+    Page<Order> searchAndFilterUsers(OrderFilterRequestDto orderFilterRequestDto, Sort.Direction direction, int pageNumber, int pageSize, String sortBy);
 }
--- src/main/java/com/example/servicesprovider/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/servicesprovider/service/impl/OrderServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.servicesprovider.service.impl;
 
 import com.example.servicesprovider.base.service.impl.BaseServiceImpl;
+import com.example.servicesprovider.dto.OrderFilterRequestDto;
 import com.example.servicesprovider.model.Order;
 import com.example.servicesprovider.model.SubService;
 import com.example.servicesprovider.repository.OrderRepository;
 import com.example.servicesprovider.service.OrderService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,5 +26,11 @@ public class OrderServiceImpl extends BaseServiceImpl<Order, Long, OrderReposito
         List<Order> orders = repository.findAllBySubServiceId(subService.getId());
         if (orders == null) throw new EntityNotFoundException("Model not exist");
         return orders;
+    }
+
+    @Override
+    @Transactional
+    public Page<Order> searchAndFilterUsers(OrderFilterRequestDto orderFilterRequestDto, Sort.Direction direction, int pageNumber, int pageSize, String sortBy) {
+        return repository.searchAndFilterUsers(orderFilterRequestDto, direction, pageNumber, pageSize, sortBy);
     }
 }
